5e0266828b56676516bd578ac8e6cff2f60cf045
amueller <amueller@microsoft.com>
2025-02-11 23:30:05 +0000

    clean up tabflex code a bit

--- ticl/prediction/tabflex.py
+++ ticl/prediction/tabflex.py
@@ -9,7 +9,6 @@ from ticl.utils import fetch_model
 class TabFlex:
     def __init__(
         self,
-        max_n_training_samples = 100000000000,
     ):
         torch.set_num_threads(1)
         fetch_model('ssm_tabpfn_b4_maxnumclasses100_modellinear_attention_numfeatures1000_n1024_validdatanew_warm_08_23_2024_19_25_40_epoch_3140.cpkt')
@@ -38,21 +37,8 @@ class TabFlex:
             epoch = '3110',
         )
 
-        self.max_n_training_samples = max_n_training_samples
-
     def fit(self, X, y):
         N, D = X.shape
-        
-        #WARNING: When overwrite_warning is true, TabPFN will attempt to run on arbitrarily large datasets! This means if you run TabPFN on a large dataset without sketching/sampling it may crash rather than issuing a warning and refusing to run
-        if X.shape[0] > self.max_n_training_samples:
-            # select indices to have as balanced a dataset as possible
-            classes = np.unique(y)
-            selected_indices = []
-            for c in classes:
-                indices = np.where(y == c)[0]
-                selected_indices.extend(np.random.choice(indices, self.max_n_training_samples // len(classes), replace=True))
-
-            X, y = X[selected_indices,:], y[selected_indices]
 
         if N >= 3000 and D <= 100:
             self.model = self.tabflexl100
